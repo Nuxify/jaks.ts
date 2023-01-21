d903de0e3126a8f8480f1f87a35c90528a8d6577
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2023-01-21 19:13:44 +0800

    feat: add localstorage repository

---
+++ api/localstorage/index.ts
@@ -0,0 +1,4 @@
+import LocalStorageRepository from './localstorage.repository'
+import { LsKeys, LsStaticValues } from './localstorage.types'
+
+export { LocalStorageRepository, LsKeys, LsStaticValues }
--- plugins/repository.ts
+++ plugins/repository.ts
@@ -1,6 +1,8 @@
+import { LocalStorageRepository } from '~/api/localstorage'
 import { SocialRepository } from '~/api/social'
 
 export default (ctx: any, inject: any) => {
   // dependency injection
+  inject('localStorageRepository', LocalStorageRepository())
   inject('socialRepository', SocialRepository(ctx.$axios))
 }
